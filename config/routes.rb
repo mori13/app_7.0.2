9ab42996dd81e851b16bd1ae6f8e45ac93faba17
mori13 <35800.abc@gmail.com>
2022-04-02 13:37:31 +0900

    remake_devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'top#index'
+  devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_in' => 'devise/sessions#new'
+    get '/users/sign_up' => 'devise/registration#new'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
